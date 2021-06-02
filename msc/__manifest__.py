f23012a1703a71f00654c24d9dc90bb5c5a07141
Nastenko Maksym <m.nastenko@parkcode.com.ua>
2021-06-02 11:51:03 +0300

    Fast receipt print button; whole sale order discount

--- msc/__manifest__.py
+++ msc/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "MSC",
-    'version': '14.0.1.4.1',
+    'version': '14.0.1.4.2',
     'category': 'CRM',
     'application': True,
     'installable': True,
