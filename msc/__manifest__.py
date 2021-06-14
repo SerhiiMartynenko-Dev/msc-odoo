a67037fde745f5e4ab125b554aec49d9d4fe5d7b
Nastenko Maksym <m.nastenko@parkcode.com.ua>
2021-06-14 08:59:44 +0300

    Add stock cost columns

--- msc/__manifest__.py
+++ msc/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "MSC",
-    'version': '14.0.1.5.1',
+    'version': '14.0.1.5.2',
     'category': 'CRM',
     'application': True,
     'installable': True,
